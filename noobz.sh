4e5e45bdaa2b895b2ee2e3a7c3da553155d104fb
gazzent <158091173+gazzent@users.noreply.github.com>
2024-02-12 08:20:29 +0700

    Update noobz.sh

--- noobz.sh
+++ noobz.sh
@@ -4,7 +4,7 @@ apt install git
 git clone https://github.com/noobz-id/noobzvpns.git
 cd noobzvpns/
 ./install.sh
-systemctl start noobzvpns.service
-systemctl restart noobzvpns.service
-systemctl enable noobzvpns.service
-journalctl -u noobzvpns.service
+systemctl start noobzvpns
+systemctl restart noobzvpns
+systemctl enable noobzvpns
+journalctl -u noobzvpns
